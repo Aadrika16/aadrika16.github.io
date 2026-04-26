32bf5b236cb82c6290b05735baf1bc4c0b9cfc4e
Aadrika16 <nemurimadhuri@gmail.com>
2026-04-26 14:39:46 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Home from './component/Home'
 import Services from './component/Services'
-import Clients from './component/Client'
+import Clients from './component/Clients'
+import Contact from './component/Contact'
 import { BrowserRouter ,Switch, Route } from 'react-router-dom'
 import './App.css'
+import About from './component/About'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/services" component = {Services} />
         <Route exact path="/clients" component = {Clients} />
+        <Route exact path="/contact" component={Contact} /> 
+        <Route exact path ="/about" component ={About} />
       </Switch>
     </BrowserRouter>
     
---
+++ src/component/About/index.js
@@ -0,0 +1,6 @@
+const About =()=>{
+    return (
+        <h1>Hi</h1>
+    )
+}
+export default About
