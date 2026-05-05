2295bac1c6290bda1297b25109a052d027394f67
Aadrika16 <nemurimadhuri@gmail.com>
2026-05-05 22:36:56 +0530

    updated site

--- src/component/RegisterForm/index.js
+++ src/component/RegisterForm/index.js
@@ -1,26 +1,49 @@
 import './index.css'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
 
 const RegisterForm = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      'service_9dqxsen',
+      'template_zmhx5y8',
+      form.current,
+      'sKXIJKNzq2gkS2Mjd'
+    )
+    .then(() => {
+      alert('Form submitted successfully ✅')
+      form.current.reset()
+      
+    })
+    .catch(() => {
+      alert('Failed to send ❌')
+    })
+  }
+
   return (
-    <form className="register-form">
+    <form ref={form} onSubmit={sendEmail} className="register-form">
       <div className="input-group">
         <label>Name</label>
-        <input type="text" placeholder="Enter Full Name" />
+        <input type="text" name="name" placeholder="Enter Full Name" required />
       </div>
 
       <div className="input-group">
         <label>Email Address</label>
-        <input type="email" placeholder="Enter Email" />
+        <input type="email" name="email" placeholder="Enter Email" required />
       </div>
 
       <div className="input-group">
         <label>Mobile Number</label>
-        <input type="text" placeholder="Enter Mobile Number" />
+        <input type="text" name="mobile" placeholder="Enter Mobile Number" required />
       </div>
 
       <div className="input-group">
         <label>Description</label>
-        <textarea placeholder="Describe Your Requirement"></textarea>
+        <textarea name="message" placeholder="Describe Your Requirement" required />
       </div>
 
       <button className="submit-btn" type="submit">
